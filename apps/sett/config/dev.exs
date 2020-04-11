4be5dd9b35b66f08da4af37f89f01daa10d409d9
Eliot Piering <eliot@eliotpiering.com>
2020-04-11 18:26:06 -0500

    Updating port for sett app

--- apps/sett/config/dev.exs
+++ apps/sett/config/dev.exs
@@ -16,7 +16,7 @@ config :sett, Sett.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :sett, SettWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
