f0ea4eb7388197ca09d7b6b97d0a06cdaf169c0a
Eliot Piering <eliot@eliotpiering.com>
2020-04-11 19:20:47 -0500

    adding version to new mix file

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule PersonalBlogUmbrella.Mixfile do
     [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     version: "1.0.1",
      deps: deps()]
   end
 
