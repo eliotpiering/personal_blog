6484383332f216854d23197a077e87790715e65d
Eliot Piering <eliot@eliotpiering.com>
2020-04-11 21:31:51 -0500

    more release shit

--- rel/config.exs
+++ rel/config.exs
@@ -47,7 +47,7 @@ end
 # will be used by default
 
 release :personal_blog do
-  set version: "1.1.1"
+  set version: "1.1.2"
   set applications: [
     :runtime_tools,
     personal_blog: :permanent,
